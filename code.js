99316ed5c8987126524fcbc0873a3bfd4a0d6930
david <>
2021-05-28 16:30:54 +0400

    fix key start with numer

--- code.js
+++ code.js
@@ -113,7 +113,8 @@ $(function () {
         if (typeof key === 'string') {
           //https://dart.dev/guides/language/language-tour
           let reservedKeywords = ["num", "double", "int", "String", "bool", "List", "abstract", "dynamic", "implements", "show", "as", "else", "import", "static", "assert", "enum", "in", "super", "async", "export", "interface", "switch", "await", "extends", "is", "sync", "break", "external", "library", "this", "case", "factory", "mixin", "throw", "catch", "false", "new", "true", "class", "final", "null", "try", "const", "finally", "on", "typedef", "continue", "for", "operator", "var", "covariant", "Function", "part", "void", "default", "get", "rethrow", "while", "deferred", "hide", "return", "with", "do", "if", "set", "yield"];
-          if (reservedKeywords.includes(key)) {
+          let isStartWithNum = key.match(/^\d/);
+          if (reservedKeywords.includes(key) || isStartWithNum) {
             return `the${uppercaseFirst(key)}`;
           }
         }
